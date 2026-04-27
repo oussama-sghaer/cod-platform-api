0a5dac33d82934763586cf2bff075912bbbe2253
oussama-sghaier-perfanalysis <oussama.sghaier@perfanalysis.consulting>
2026-04-27 10:11:59 +0100

    chore: multi-stage dockerfile for api

--- src/me/me.controller.ts
+++ src/me/me.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get } from "@nestjs/common"
 import { CurrentContext } from "../auth/decorators/current-context.decorator"
-import { AuthContext } from "../auth/auth.types"
+import * as AuthTypes from "../auth/auth.types"
 
 @Controller("me")
 export class MeController {
   @Get()
-  me(@CurrentContext() ctx: AuthContext) {
+  me(@CurrentContext() ctx: AuthTypes.AuthContext) {
     return ctx
   }
 }
